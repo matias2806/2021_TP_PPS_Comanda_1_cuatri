7ab59aecf5ba10cf600f7aa14b916841ef979253
Shilua <Shilua4@gmail.com>
2021-05-26 19:02:15 -0300

    ionic cordova app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyC2jqL_OWT9o957k3R-xZlSbFJI_ruNVrk",
-    authDomain: "restaurantapp-3405c.firebaseapp.com",
-    projectId: "restaurantapp-3405c",
-    storageBucket: "restaurantapp-3405c.appspot.com",
-    messagingSenderId: "599887408534",
-    appId: "1:599887408534:web:3e5bbcc0deaf859914c1b7"
-  },
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyC2jqL_OWT9o957k3R-xZlSbFJI_ruNVrk",
-    authDomain: "restaurantapp-3405c.firebaseapp.com",
-    projectId: "restaurantapp-3405c",
-    storageBucket: "restaurantapp-3405c.appspot.com",
-    messagingSenderId: "599887408534",
-    appId: "1:599887408534:web:3e5bbcc0deaf859914c1b7"
-  },
+  production: false
 };
 
 /*
